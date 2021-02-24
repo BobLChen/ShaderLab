66a67510ca910ffac17d4c511ca1a1e576097787
BobLCheno <chenbo150928@gmail.com>
2021-02-24 12:07:41 +0800

    禁用OPT

--- ThirdParty/glslang.cmake
+++ ThirdParty/glslang.cmake
@@ -6,6 +6,7 @@ set(SKIP_GLSLANG_INSTALL ON CACHE BOOL "" FORCE)
 set(ENABLE_GLSLANG_BINARIES OFF CACHE BOOL "" FORCE)
 set(ENABLE_HLSL OFF CACHE BOOL "" FORCE)
 set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
+set(ENABLE_OPT OFF CACHE BOOL "" FORCE)
 
 add_subdirectory(glslang)
 foreach(target "glslang" "OGLCompiler" "OSDependent" "SPIRV" "SPVRemapper")
